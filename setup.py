96c1affa3dd9d08153e1a937d0d5ae6d5561b907
alcubierre-drive <alcubierre-drive>
2025-09-11 09:19:35 +0200

    ybm

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
    name='fancymaps',
-   version='v0.1.19',
+   version='v0.1.20',
    description='Visually appealing (diverging) colormaps',
    long_description_content_type = 'text/markdown',
    packages=['fancymaps'],
