9edf03ba4b1bc3de4aa0c852afae3fa750a28277
alcubierre-drive <alcubierre-drive>
2025-11-17 17:40:43 +0100

    version bump

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
    name='fancymaps',
-   version='v0.1.21',
+   version='v0.1.22',
    description='Visually appealing (diverging) colormaps',
    long_description_content_type = 'text/markdown',
    packages=['fancymaps'],
